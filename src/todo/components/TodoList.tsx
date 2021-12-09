f59032932fc21fe8551061145f121c9d5d0d975c
alvaro-78 <alvaro78an@gmail.com>
2021-12-09 17:16:49 +0100

    TodoList

--- src/todo/components/TodoList.tsx
+++ src/todo/components/TodoList.tsx
@@ -1,11 +1,8 @@
-import { useContext } from 'react';
-import { TodoContext } from '../context/TodoContext';
 import { TodoItems } from './TodoItems';
+import { useTodos } from '../hooks/useTodos';
 
 export const TodoList = () => {
-	const { todoState } = useContext(TodoContext);
-
-	const { todos } = todoState;
+	const { todos } = useTodos();
 
 	return (
 		<>
